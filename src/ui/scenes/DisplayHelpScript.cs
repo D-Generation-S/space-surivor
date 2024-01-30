0f0e60c2f29f758ecaadcd766a2680feee7ca2ae
XanatosX <10531466+XanatosX@users.noreply.github.com>
2024-01-30 22:35:07 +0100

    Add missing doc blocks Fixed help menu

--- src/ui/scenes/DisplayHelpScript.cs
+++ src/ui/scenes/DisplayHelpScript.cs
@@ -1,61 +1,69 @@
 using Godot;
-using System;
-using System.Linq;
-using System.Runtime.InteropServices;
 
+/// <summary>
+/// Script to display the help screen
+/// It will cycle the screen entries
+/// </summary>
 public partial class DisplayHelpScript : Control
 {
+	/// <summary>
+	/// All the help screens available
+	/// </summary>
 	[Export]
 	private HelpPanel[] helpScenes;
 
-	private bool firstShown = false;
-
+	/// <summary>
+	/// The current index
+	/// </summary>
 	private int index;
 
-	private int previousIndex;
-
-	private HelpPanel firstHelpScene;
+	/// <summary>
+	/// The currently shown window
+	/// </summary>
+	private int currentlyShownWindow;
 
     public override void _Ready()
     {
-        firstHelpScene = helpScenes.FirstOrDefault();
-		index = 0;
-		previousIndex = 0;
+		index = -1;
+		currentlyShownWindow = 0;
 		MouseFilter = MouseFilterEnum.Ignore;
     }
 
+	/// <summary>
+	/// Hide the previous help text and show the next one
+	/// </summary>
     public void NextHelpText()
-	{
-		if (!firstShown)
-		{
-			firstHelpScene.ShowingPanel();
-			firstShown = true;
-			index++;
-			index = index >= helpScenes.Length ? 0 : index;
-
-			return;
-		}
-		
-
-		helpScenes[previousIndex].HidingPanel();
-		previousIndex = index;
-		helpScenes[index].ShowingPanel();
+	{		
+		helpScenes[currentlyShownWindow].HidingPanel();
 		index++;
-		index = index >= helpScenes.Length ? 0 : index;
+		index = index >= helpScenes.Length ? helpScenes.Length - 1 : index;
+
+		helpScenes[index].ShowingPanel();
+		currentlyShownWindow = index;
 	}
 
+	/// <summary>
+	/// Hide the previous help text and show the one before one
+	/// </summary>
 	public void PreviousHelpText()
 	{
-		helpScenes[previousIndex].HidingPanel();
-		previousIndex = index;
-		helpScenes[index].ShowingPanel();
+		helpScenes[currentlyShownWindow].HidingPanel();
 		index--;
-		index = index < 0 ? helpScenes.Length - 1 : index;
+		if (index < 0)
+		{
+			GD.PushError("Trying to show window with negative index!");
+			return;
+		}
+		helpScenes[index].ShowingPanel();
+		currentlyShownWindow = index;
 	}
 
+	/// <summary>
+	/// Close the help panels and reset the index
+	/// </summary>
 	public void CloseHelpPanels()
 	{
-		helpScenes[previousIndex].HidingPanel();
-		index = 0;
+		helpScenes[currentlyShownWindow].HidingPanel();
+		index = -1;
 	}
 }

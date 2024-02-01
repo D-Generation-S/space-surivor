2d03de26115ef150cb094423a22dfaa2cdf75d4e
XanatosX <10531466+XanatosX@users.noreply.github.com>
2024-02-01 22:34:17 +0100

    Add missing summary blocks

--- src/resources/EngineConfiguration.cs
+++ src/resources/EngineConfiguration.cs
@@ -1,47 +1,70 @@
 using Godot;
 
+/// <summary>
+/// The configuration for an ship engine
+/// </summary>
 [GlobalClass]
 public partial class EngineConfiguration : BaseComponent
 {
-
+    /// <summary>
+    /// The acceleration of this engine.
+    /// The y component does represent forward and backward,
+    /// while x represents the strafing speed
+    /// </summary>
     [Export]
     private Vector2 engineAcceleration;
     
-
+    /// <summary>
+    /// The rotation speed for this engine
+    /// </summary>
     [Export]
     private float engineRotation;
 
-    [Export]
-    private int engineSize;
-
+    /// <summary>
+    /// The consumption for this engine
+    /// </summary>
     [Export]
     private ConsumerConfiguration consumerConfiguration;
 
+    /// <summary>
+    /// The heat configuration
+    /// </summary>
     [Export]
     private HeatConfiguration heatConfiguration;
 
+    /// <summary>
+    /// Get the consumer configuration
+    /// </summary>
+    /// <returns>The consumption configuration for the engine</returns>
     public ConsumerConfiguration GetConsumerConfiguration()
     {
         return consumerConfiguration;
     }
 
+    /// <summary>
+    /// Get the heat configuration
+    /// </summary>
+    /// <returns>The heat configuration for this engine</returns>
     public HeatConfiguration GetHeatConfiguration()
     {
         return heatConfiguration;
     }
 
+    /// <summary>
+    /// Get the acceleration of this engine
+    /// </summary>
+    /// <returns></returns>
     public Vector2 GetEngineAcceleration()
     {
         return engineAcceleration;
     }
 
+    /// <summary>
+    /// Get the rotation speed of this engine
+    /// </summary>
+    /// <returns>The rotation speed</returns>
     public float GetEngineRotationSpeed()
     {
         return engineRotation;
-    }
-
-    public int GetEngineSize()
-    {
-        return engineSize;
     }
 }
--- src/resources/HeatConfiguration.cs
+++ src/resources/HeatConfiguration.cs
@@ -1,14 +1,21 @@
 using Godot;
 
+/// <summary>
+/// Heat configuration for a given component
+/// </summary>
 [GlobalClass]
 public partial class HeatConfiguration : Resource
 {
     /// <summary>
-    /// The name of the input
+    /// The heat which is generated per tick
     /// </summary>
     [Export]
     private int heatPerTick;
 
+    /// <summary>
+    /// Get the heat generated per tick
+    /// </summary>
+    /// <returns></returns>
     public int GetHeatPerTick()
     {
         return heatPerTick;

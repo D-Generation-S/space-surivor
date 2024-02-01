753ddb3d05d9da6c2fcd30196d4a4da01d452910
XanatosX <10531466+XanatosX@users.noreply.github.com>
2024-02-01 21:51:40 +0100

    Add missing summary blocks

--- src/entities/components/CoolingUnitComponent.cs
+++ src/entities/components/CoolingUnitComponent.cs
@@ -2,25 +2,51 @@ using System;
 using System.Linq;
 using Godot;
 
+/// <summary>
+/// Component to cool down the ship,
+/// this will fetch heat from the ship's component and cool it down.
+/// If the stored head exceeds the capacity it will damage the ship hull
+/// </summary>
 public partial class CoolingUnitComponent : ConsumerComponent
 {
+	/// <summary>
+	/// Event if the max heat capacity did change
+	/// </summary>
+	/// <param name="newHeatCapacity"></param>
     [Signal]
     public delegate void MaxHeatCapacityChangedEventHandler(int newHeatCapacity);
     
+	/// <summary>
+	/// Event if the stored heat capacity did change
+	/// </summary>
+	/// <param name="newHeat"></param>
     [Signal]
     public delegate void StoredHeatChangedEventHandler(int newHeat);
 
+	/// <summary>
+	/// The configuration of this cooling component
+	/// </summary>
 	[Export]
 	private CoolingUnitConfiguration coolingUnitConfiguration;
 
+	/// <summary>
+	/// The max heat damage per tick done by this cooling component
+	/// </summary>
 	[Export]
 	private int maxHeatDamage = 5;
 
+	/// <summary>
+	/// The currently stored heat amount
+	/// </summary>
 	private int storedHeat;
 
+	/// <summary>
+	/// The health component of the ship to damage
+	/// </summary>
 	private HealthComponent healthComponent;
 
-	// Called when the node enters the scene tree for the first time.
+
+	/// <inheritdoc/>
 	public override void _Ready()
 	{
 		storedHeat = 0;
@@ -31,6 +57,7 @@ public partial class CoolingUnitComponent : ConsumerComponent
 		}
 	}
 
+	/// <inheritdoc/>
     public override void ConsumerTick(int tickNumber)
     {
         var consumers = GetParent().GetChildren().OfType<ConsumerComponent>().Where(consumer => consumer != this).ToList();
@@ -53,21 +80,28 @@ public partial class CoolingUnitComponent : ConsumerComponent
 		EmitSignal(SignalName.StoredHeatChanged, storedHeat);
     }
 
+	/// <summary>
+	/// Get the maximal heat capacity of this cooling component
+	/// </summary>
+	/// <returns>The max heat capacity</returns>
     public int GetMaxHeatCapacity()
 	{
 		return coolingUnitConfiguration.GetHeatCapacity();
 	}
 
+/// <inheritdoc/>
     public override int GetStoredHeat()
     {
         return storedHeat;
     }
 
+/// <inheritdoc/>
     public override int GetConsumption()
     {
         return coolingUnitConfiguration.GetConsumerConfiguration().GetConsumption();
     }
 
+/// <inheritdoc/>
 	public override int GetPriority()
     {
         return coolingUnitConfiguration.GetConsumerConfiguration().GetPriority();
--- src/entities/components/EngineComponent.cs
+++ src/entities/components/EngineComponent.cs
@@ -1,14 +1,29 @@
 using Godot;
 
+/// <summary>
+/// The engine component of the ship
+/// </summary>
 public partial class EngineComponent : ConsumerComponent
 {
+    /// <summary>
+    /// The engine configuration for this component
+    /// </summary>
     [Export]
     private EngineConfiguration engineConfiguration;
 
+    /// <summary>
+    /// The current heat of the engine
+    /// </summary>
     private int currentHeat;
 
+    /// <summary>
+    /// The last power consumption in the previous tick of this engine
+    /// </summary>
     private int lastPowerConsumption;
 
+    /// <summary>
+    /// was the engine on at the last tick
+    /// </summary>
     private bool wasOn;
 
 	// Called when the node enters the scene tree for the first time.
@@ -17,6 +32,7 @@ public partial class EngineComponent : ConsumerComponent
         currentHeat = 0;
 	}
 
+    /// <inheritdoc/>
     public override void ConsumerTick(int tickNumber)
     {
         if (!wasOn)
@@ -29,11 +45,15 @@ public partial class EngineComponent : ConsumerComponent
         wasOn = false;
     }
 
+    /// <summary>
+    /// Is the engine currently firing
+    /// </summary>
     public void Firing()
     {
         wasOn = true;
     }
 
+/// <inheritdoc/>
     public override int GetStoredHeat()
     {
         var returnHeat = currentHeat;
@@ -41,21 +61,33 @@ public partial class EngineComponent : ConsumerComponent
         return returnHeat;
     }
 
+/// <inheritdoc/>
     public override int GetConsumption()
     {
         return lastPowerConsumption;
     }
 
+/// <inheritdoc/>
     public override int GetPriority()
     {
         return engineConfiguration.GetConsumerConfiguration().GetPriority();
     }
 
+    /// <summary>
+    /// The speed this engine can accelerate with
+    /// the y component does represent forward and backward, 
+    /// while x represents strafing
+    /// </summary>
+    /// <returns>The vector with the acceleration of the ship</returns>
     public Vector2 GetAccelerationSpeed()
     {
         return engineConfiguration.GetEngineAcceleration();
     }
 
+    /// <summary>
+    /// The speed this engine can rotate the ship with
+    /// </summary>
+    /// <returns>The rotation speed</returns>
     public float GetRotationSpeed()
     {
         return engineConfiguration.GetEngineRotationSpeed();

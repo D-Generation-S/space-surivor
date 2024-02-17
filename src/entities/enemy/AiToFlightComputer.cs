3eaa7217b728c60476e573345b6784f85a9e051c
XanatosX <10531466+XanatosX@users.noreply.github.com>
2024-02-17 13:18:41 +0100

    Fix formatting

--- src/entities/enemy/AiToFlightComputer.cs
+++ src/entities/enemy/AiToFlightComputer.cs
@@ -7,86 +7,86 @@ using Godot;
 /// </summary>
 public partial class AiToFlightComputer : Node
 {	
-	/// <summary>
-	/// The flight computer of this ai entity
-	/// </summary>
-	[Export]
-	private FlightComputer flightComputer;
+    /// <summary>
+    /// The flight computer of this ai entity
+    /// </summary>
+    [Export]
+    private FlightComputer flightComputer;
 
-	/// <summary>
-	/// The entity controlled by this bridge
-	/// </summary>
-	[Export]
-	private EntityMovement entityMovement;
+    /// <summary>
+    /// The entity controlled by this bridge
+    /// </summary>
+    [Export]
+    private EntityMovement entityMovement;
 
-	/// <summary>
+    /// <summary>
     /// The allowed difference between target and real rotation, this prevents some osculation
     /// </summary>
     [Export]
     private float rotationDifference = 0.001f;
 
-	/// <summary>
-	/// The global flight direction to achieve
-	/// </summary>
-	private Vector2 globalFlightDirectionTarget;
+    /// <summary>
+    /// The global flight direction to achieve
+    /// </summary>
+    private Vector2 globalFlightDirectionTarget;
 
-	/// <summary>
-	/// The global target rotation to achieve
-	/// </summary>
-	private float globalTargetRotation;
+    /// <summary>
+    /// The global target rotation to achieve
+    /// </summary>
+    private float globalTargetRotation;
 
-	/// <summary>
-	/// The speed to accelerate
-	/// </summary>
-	private float speed;
+    /// <summary>
+    /// The speed to accelerate
+    /// </summary>
+    private float speed;
 
     public override void _Ready()
     {
         globalFlightDirectionTarget = Vector2.Zero;
-		speed = 0;
+        speed = 0;
     }
 
     public override void _PhysicsProcess(double delta)
     {
-		SetRotation();
-		SpeedUp();
+        SetRotation();
+        SpeedUp();
     }
 
-	/// <summary>
-	/// Set the global target velocity
-	/// this method will set the rotation automatically
-	/// </summary>
-	/// <param name="velocity">The new global target verlocity</param>
-	public void SetGlobalVelocity(Vector2 velocity)
-	{
-		globalFlightDirectionTarget = velocity.Normalized();
-		globalTargetRotation = globalFlightDirectionTarget.AngleTo(Vector2.Up) * -1;
-	}
+    /// <summary>
+    /// Set the global target velocity
+    /// this method will set the rotation automatically
+    /// </summary>
+    /// <param name="velocity">The new global target verlocity</param>
+    public void SetGlobalVelocity(Vector2 velocity)
+    {
+        globalFlightDirectionTarget = velocity.Normalized();
+        globalTargetRotation = globalFlightDirectionTarget.AngleTo(Vector2.Up) * -1;
+    }
 
-	/// <summary>
-	/// Set the speed to accelerate the ship	
-	/// </summary>
-	/// <param name="speedPercentage">The percentage to accelerate, should be between 0 and 1</param>
-	public void SetSpeed(float speedPercentage)
-	{
-		speed = Math.Clamp(speedPercentage, 0, 1);
-	}
+    /// <summary>
+    /// Set the speed to accelerate the ship	
+    /// </summary>
+    /// <param name="speedPercentage">The percentage to accelerate, should be between 0 and 1</param>
+    public void SetSpeed(float speedPercentage)
+    {
+        speed = Math.Clamp(speedPercentage, 0, 1);
+    }
 
-	/// <summary>
-	/// Get the entity which is controlled by this ai to flight computer instance
-	/// </summary>
-	/// <returns>The entity movement which is controlled</returns>
-	public EntityMovement GetControlledEntity()
-	{
-		return entityMovement;
-	}
+    /// <summary>
+    /// Get the entity which is controlled by this ai to flight computer instance
+    /// </summary>
+    /// <returns>The entity movement which is controlled</returns>
+    public EntityMovement GetControlledEntity()
+    {
+        return entityMovement;
+    }
 
-	/// <summary>
-	/// Rotate the ship to the planned global target rotation
-	/// </summary>
-	private void SetRotation()
-	{
-		var diff = globalTargetRotation - entityMovement.GlobalRotation;
+    /// <summary>
+    /// Rotate the ship to the planned global target rotation
+    /// </summary>
+    private void SetRotation()
+    {
+        var diff = globalTargetRotation - entityMovement.GlobalRotation;
         var absoluteDiff = Math.Abs(diff);
         if (absoluteDiff > FloatExtension.HALF_ROTATION)
         {
@@ -102,7 +102,7 @@ public partial class AiToFlightComputer : Node
         float direction = diff < 0 ? -1f : 1f;
         if (diff > 1)
         {
-			flightComputer.CommandRotation(direction);
+            flightComputer.CommandRotation(direction);
             return;
         }
         float targetTurnRate = FloatExtension.Lerp(clamped, 0, 0.04f) * direction;
@@ -111,14 +111,14 @@ public partial class AiToFlightComputer : Node
         float percentage = Math.Clamp(Math.Abs(turnDifference / targetTurnRate), 0, 1);
         direction = turnDifference > 0 ? 1f : -1f;
         
-		flightComputer.CommandRotation(percentage.Lerp(0, 1) * direction);
-	}
+        flightComputer.CommandRotation(percentage.Lerp(0, 1) * direction);
+    }
 
-	/// <summary>
-	/// Accelerate the ship forward by the speed percentage given
-	/// </summary>
-	private void SpeedUp()
-	{
-		flightComputer.CommandBaseFlightVector(Vector2.Up * speed);
-	}
+    /// <summary>
+    /// Accelerate the ship forward by the speed percentage given
+    /// </summary>
+    private void SpeedUp()
+    {
+        flightComputer.CommandBaseFlightVector(Vector2.Up * speed);
+    }
 }

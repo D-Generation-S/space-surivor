88156d6d1d95e4d76b54786627201651d31e5af4
Xanatos <10531466+XanatosX@users.noreply.github.com>
2024-02-18 13:38:59 +0100

    Add missing summary blocks

--- src/ui/overlay/PauseMenu.cs
+++ src/ui/overlay/PauseMenu.cs
@@ -1,14 +1,22 @@
 using Godot;
-using System;
 
+/// <summary>
+/// Class to control the pause menu
+/// </summary>
 public partial class PauseMenu : CanvasLayer
 {
+	/// <summary>
+	/// Method to display the pause menu
+	/// </summary>
 	public void ShowMenu()
 	{
 		Visible = true;
 		GetTree().Paused = true;
 	}
 
+	/// <summary>
+	/// Method to hide the pause menu
+	/// </summary>
 	public void HideMenu()
 	{
 		Visible = false;

faf7ff470c7ee955f95e563175a6302c32d8e497
Xanatos <10531466+XanatosX@users.noreply.github.com>
2024-02-21 23:39:23 +0100

    Fix formatting

--- src/stateMachine/aiBehavior/StopState.cs
+++ src/stateMachine/aiBehavior/StopState.cs
@@ -1,35 +1,34 @@
 using Godot;
-using System;
 
 public partial class StopState : State
 {
 
-	[Export]
-	private CharacterBody2D controlledBody;
+    [Export]
+    private CharacterBody2D controlledBody;
 
-	[Export]
-	private State nextState;
+    [Export]
+    private State nextState;
 
     public override void Enter()
     {
-		if (controlledBody is null)
-		{
-			GD.PushError("Missing controlled body");
-			return;
-		}
+        if (controlledBody is null)
+        {
+            GD.PushError("Missing controlled body");
+            return;
+        }
         controlledBody.Velocity = Vector2.Zero;
-		
+        
         if (nextState is not null)
-		{
-			EmitSignal(SignalName.Transitioned, this, nextState.GetType().Name);
-		}
+        {
+            EmitSignal(SignalName.Transitioned, this, nextState.GetType().Name);
+        }
     }
 
     public override void Update(double delta)
     {
         if (nextState is not null)
-		{
-			EmitSignal(SignalName.Transitioned, this, nextState.GetType().Name);
-		}
+        {
+            EmitSignal(SignalName.Transitioned, this, nextState.GetType().Name);
+        }
     }
 }

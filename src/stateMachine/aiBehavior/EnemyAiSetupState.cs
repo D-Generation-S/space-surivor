722388ac70a8f62c527d554fe5a30cd46a0f2973
Xanatos <10531466+XanatosX@users.noreply.github.com>
2024-02-22 21:10:26 +0100

    Add missing summary blocks

--- src/stateMachine/aiBehavior/EnemyAiSetupState.cs
+++ src/stateMachine/aiBehavior/EnemyAiSetupState.cs
@@ -1,16 +1,29 @@
 using Godot;
 
+/// <summary>
+/// A initial state to setup a blackboard for a FSM
+/// </summary>
 public partial class EnemyAiSetupState : State
 {
+    /// <summary>
+    /// The blackboard key for the ai to flight computer adapter
+    /// </summary>
     [Export]
     private string aiToFlightComputerBlackboardKey = "aiToFlightComputer";
 
+    /// <summary>
+    /// The ai to flight computer to use
+    /// </summary>
     [Export]
     private AiToFlightComputer aiToFlightComputer;
 
+    /// <summary>
+    /// The next state to went to if this state is completed
+    /// </summary>
     [Export]
     private State nextState;
 
+    /// <inheritdoc />
     public override void Enter()
     {
         if (blackboard is null)
